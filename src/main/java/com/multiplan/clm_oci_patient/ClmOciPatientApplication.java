516f03cf60ec7218e2f4126b95dd09a2ea4d5db0
Pal, Trina <Trina.Pal+mpln@claritev.com>
2025-08-11 11:34:28 -0700

    adding patient details structure

---
+++ src/main/java/com/multiplan/clm_oci_patient/ClmOciPatientApplication.java
@@ -0,0 +1,13 @@
+package com.multiplan.clm_oci_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClmOciPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClmOciPatientApplication.class, args);
+	}
+
+}
